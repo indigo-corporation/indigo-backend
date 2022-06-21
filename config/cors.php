f51f084f4a83cde710e706b64e083f81c26fcfba
kirill2012 <k.yevchenko@ukr.net>
2022-06-21 16:23:53 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['https://indigo-front.herokuapp.com/'],
+    'allowed_origins' => ['https://indigo-front.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
