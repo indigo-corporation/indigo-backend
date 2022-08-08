f3cb8abdf817768becac8fa5f3b0f6248a2f04fd
kirill2012 <k.yevchenko@ukr.net>
2022-08-08 12:41:48 +0300

    truncate cities

--- database/seeders/CitiesSeeder.php
+++ database/seeders/CitiesSeeder.php
@@ -10,5 +10,20 @@ class CitiesSeeder extends Seeder
     public function run()
     {
         DB::table('cities')->truncate();
+
+        DB::table('cities')->insert([
+            [
+                'country_id' => 233,
+                'state_id' =>  4380,
+                'name' => 'Kyiv',
+                'country_code' => 'UA'
+            ],
+            [
+                'country_id' => 233,
+                'state_id' =>  4380,
+                'name' => 'Not Kyiv',
+                'country_code' => 'UA'
+            ]
+        ]);
     }
 }
