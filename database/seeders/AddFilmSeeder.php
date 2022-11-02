d1dcc884e7edeb6a0d9deddb2214aa8ed60e9815
Kirill <k.yevchenko@ukr.net>
2022-11-02 20:18:21 +0200

    delete film

--- database/seeders/AddFilmSeeder.php
+++ database/seeders/AddFilmSeeder.php
@@ -9,16 +9,18 @@ class AddFilmSeeder extends Seeder
 {
     public function run()
     {
-        $film = new Film([
-            'original_title' => 'Druk',
-            'ru' => [
-                'title' => 'Еще по одной',
-                'overview' => 'Four high-school teachers consume alcohol on a daily basis to see how it affects their social and professional lives.',
-            ],
-            'imdb_id' => 'tt10288566',
-            'year'=> 2020
-        ]);
+//        $film = new Film([
+//            'original_title' => 'Druk',
+//            'ru' => [
+//                'title' => 'Еще по одной',
+//                'overview' => 'Four high-school teachers consume alcohol on a daily basis to see how it affects their social and professional lives.',
+//            ],
+//            'imdb_id' => 'tt10288566',
+//            'year'=> 2020
+//        ]);
+//
+//        $film->save();
 
-        $film->save();
+        Film::find(201)->delete();
     }
 }
