08a6f2f36623293b4f7fad3519d9f73aecd0abdc
Kirill <k.yevchenko@ukr.net>
2022-11-04 20:54:23 +0200

    telegram auth

--- app/Http/ApiControllers/AuthController.php
+++ app/Http/ApiControllers/AuthController.php
@@ -186,26 +186,53 @@ class AuthController extends Controller
 
     public function telegramAuth(Request $request)
     {
+        $userData = $this->checkTelegramAuthorization($request->get('data'));
 
+        $user = User::where('telegram_id', $userData['id'])->first();
+
+        if (!$user) {
+            $firstName = $userData['first_name'] ?? '';
+            $lastName = $userData['last_name'] ?? '';
+            $name = $lastName
+                ? $lastName . ' ' . $firstName
+                : $firstName;
+
+            $user = new User();
+            $user->name = $name;
+            $user->user_name = $userData['username'] ?? '';
+            $user->telegram_id = $userData['id'];
+
+            $user->save();
+        }
+
+        if (!$user->user_name) {
+            $user->user_name = 'user' . $user->id;
+            $user->save();
+        }
+
+        return response()->success([
+            'access_token' => $user->createToken('api')->plainTextToken
+        ]);
     }
 
-//    function checkTelegramAuthorization($auth_data) {
-//        $check_hash = $auth_data['hash'];
-//        unset($auth_data['hash']);
-//        $data_check_arr = [];
-//        foreach ($auth_data as $key => $value) {
-//            $data_check_arr[] = $key . '=' . $value;
-//        }
-//        sort($data_check_arr);
-//        $data_check_string = implode("\n", $data_check_arr);
-//        $secret_key = hash('sha256', BOT_TOKEN, true);
-//        $hash = hash_hmac('sha256', $data_check_string, $secret_key);
-//        if (strcmp($hash, $check_hash) !== 0) {
-//            throw new Exception('Data is NOT from Telegram');
-//        }
-//        if ((time() - $auth_data['auth_date']) > 86400) {
-//            throw new Exception('Data is outdated');
-//        }
-//        return $auth_data;
-//    }
+    private function checkTelegramAuthorization($auth_data) {
+        $check_hash = $auth_data['hash'];
+        unset($auth_data['hash']);
+        $data_check_arr = [];
+        foreach ($auth_data as $key => $value) {
+            $data_check_arr[] = $key . '=' . $value;
+        }
+        sort($data_check_arr);
+        $data_check_string = implode("\n", $data_check_arr);
+        $secret_key = hash('sha256', env('TELEGRAM_TOKEN'), true);
+        $hash = hash_hmac('sha256', $data_check_string, $secret_key);
+        if (strcmp($hash, $check_hash) !== 0) {
+            throw new Exception('Data is NOT from Telegram');
+        }
+        if ((time() - $auth_data['auth_date']) > 86400) {
+            throw new Exception('Data is outdated');
+        }
+
+        return $auth_data;
+    }
 }
