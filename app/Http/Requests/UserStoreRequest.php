39a21eb14738d1e58520cd09f37c5a18a4d5db22
Kirill <k.yevchenko@ukr.net>
2022-11-10 22:14:05 +0200

    user_name

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -10,7 +10,7 @@ class UserStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
+            'name' => 'string|min:2|max:50',
             'user_name' => 'string|min:2|unique:users,user_name',
             'birth_date' => 'date',
             'about' => 'string',
