04cdde6a0033a1322e71fdd168748c286a8ff9b6
Kirill <k.yevchenko@ukr.net>
2022-11-30 18:15:39 +0200

    sockets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\MessageReceived;
+use App\Models\Message;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-event', function () {
+    $message = Message::first();
+
+    if (!$message) return;
+
+    MessageReceived::dispatch($message);
+    return 'success';
 });
