ad7ab18609e92c8a8f7158b6ed4674178a70a904
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-03-09 20:50:05 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
 
-    //    'allowed_origins' => ['indigofilms.online'],
+//    'allowed_origins' => ['indigofilms.online'],
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
