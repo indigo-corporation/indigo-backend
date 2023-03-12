065de34c3b87a2df191a5f59169a264f17958d3b
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-03-12 02:38:54 +0200

    film posters

--- app/Console/Commands/StorePosters.php
+++ app/Console/Commands/StorePosters.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Film\Film;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 
 class StorePosters extends Command
 {
@@ -14,12 +14,24 @@ class StorePosters extends Command
 
     public function handle()
     {
+        dump(Film::whereNull('poster_medium')->count() . ' left');
+
         $i = 0;
-        DB::table('films')->chunk(100, function (Collection $films) use (&$i) {
+        Film::whereNull('poster_medium')->orderBy('id')->chunk(100, function (Collection $films) use (&$i) {
             foreach ($films as $film) {
-                $film->savePosterThumbs($film->poster);
+                try {
+                    $film->savePosterThumbs($film->poster);
+                } catch (\Throwable $e) {
+                    dump([
+                        'id' => $film->id,
+                        'error' => $e->getMessage(),
+                    ]);
+
+                    sleep(10);
+                }
             }
-            dump(++$i * 100);
+            dump('processed ' . ++$i * 100);
+            dump(Film::whereNull('poster_medium')->count() . ' left');
         });
 
         dump('completed');
