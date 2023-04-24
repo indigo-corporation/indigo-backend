46536966bd797c197dc78097ef07e512f38660b0
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-04-24 22:58:33 +0300

    prevent lazy load

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Facades\Response;
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::preventLazyLoading(!app()->isProduction());
+
         JsonResource::withoutWrapping();
         ResourceCollection::withoutWrapping();
 
