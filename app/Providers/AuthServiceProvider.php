5f5546521d51730779065f2a7410ba7e2f6af172
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-06-15 16:00:23 +0300

    reset pass

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (User $user, string $token) {
-            return 'https://indigofilms.online/reset-password?token=' . $token;
+            return 'https://indigofilms.online/reset-password/' . $token;
         });
     }
 }
