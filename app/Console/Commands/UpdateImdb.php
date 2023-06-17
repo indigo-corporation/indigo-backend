9ae4132f766a5eb32f149ce00df09650f5f4726e
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-06-17 10:00:13 +0300

    imdb job

--- app/Console/Commands/UpdateImdb.php
+++ app/Console/Commands/UpdateImdb.php
@@ -2,11 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\UpdateImdbJob;
 use App\Models\Film\Film;
-use App\Services\GetFromUrlService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
 
 class UpdateImdb extends Command
 {
@@ -17,8 +16,6 @@ class UpdateImdb extends Command
 
     public function handle()
     {
-        $getService = new GetFromUrlService();
-
         dump(
             Film::whereNotNull('imdb_id')
                 ->whereNull('imdb_votes')
@@ -30,38 +27,13 @@ class UpdateImdb extends Command
         Film::whereNotNull('imdb_id')
             ->whereNull('imdb_votes')
             ->orderBy('id', 'desc')
-            ->chunk(100, function (Collection $films) use (&$i, $getService) {
+            ->chunk(100, function (Collection $films) use (&$i) {
                 foreach ($films as $film) {
-                    $imdbData = $getService->getImdb($film->imdb_id, true);
-
-                    if (!$imdbData) {
-                        continue;
-                    }
-
-                    dump($film->imdb_id);
-
-                    try {
-                        $rating = $imdbData->rating() ?: null;
-                        $votes = $imdbData->votes();
-                    } catch (\Throwable $e) {
-                        if (Str::contains($e->getMessage(), 'Status code [404]')) {
-                            dump('not found');
-
-                            continue;
-                        }
-
-                        throw $e;
-                    }
-
-                    $film->imdb_rating = $rating;
-                    $film->imdb_votes = $votes;
-                    $film->save();
-
-                    sleep(4);
+                    UpdateImdbJob::dispatch($film);
                 }
 
                 dump('processed ' . ++$i * 100);
-                sleep(16);
+                sleep(30);
             });
     }
 }
---
+++ app/Jobs/UpdateImdbJob.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Film\Film;
+use App\Services\GetFromUrlService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
+
+class UpdateImdbJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    private Film $film;
+
+    private GetFromUrlService $getService;
+
+    public function __construct(string $film)
+    {
+        $this->film = $film;
+        $this->getService = new GetFromUrlService();
+    }
+
+    public function handle()
+    {
+        $imdbData = $this->getService->getImdb($this->film->imdb_id, true);
+
+        if (!$imdbData) {
+            return;
+        }
+
+        dump($this->film->imdb_id);
+
+        try {
+            $rating = $imdbData->rating() ?: null;
+            $votes = $imdbData->votes();
+        } catch (\Throwable $e) {
+            if (Str::contains($e->getMessage(), 'Status code [404]')) {
+                dump('not found');
+
+                return;
+            }
+
+            throw $e;
+        }
+
+        $this->film->imdb_rating = $rating;
+        $this->film->imdb_votes = $votes;
+        $this->film->save();
+    }
+}
