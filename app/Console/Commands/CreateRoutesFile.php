e7a80acf54fb43adddb5547c20e1c667d112ad2f
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-06-19 06:37:40 +0300

    create routes file

--- app/Console/Commands/CreateRoutesFile.php
+++ app/Console/Commands/CreateRoutesFile.php
@@ -5,50 +5,61 @@ namespace App\Console\Commands;
 use App\Models\Film\Film;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Process;
 
 class CreateRoutesFile extends Command
 {
-    protected $signature = 'create-routes-file';
+        protected $signature = 'create-routes-file';
 
     protected $description = 'create-routes-file';
 
 
     public function handle()
     {
-        $fileName = '/var/www/indigofilms.online/routes.txt';
+        $path = '/var/www/indigofilms.online';
+        $fileName = 'routes.txt';
 
-        file_put_contents($fileName, '');
+        file_put_contents($path . '/' . $fileName, '');
 
-        $fp = fopen($fileName, 'a+');
-        fwrite($fp, implode("\r\n", [
-            '/',
-            '/film',
-            '/anime',
-            '/serial',
-            '/cartoon',
-            '/search-page',
-            '/support',
-            '/copyright',
-            '/send-reset-password',
-            '/404'
-        ]));
-        fclose($fp);
+//        $fp = fopen($path . '/' . $fileName, 'a+');
+//        fwrite($fp, implode("\r\n", [
+//            '/',
+//            '/film',
+//            '/anime',
+//            '/serial',
+//            '/cartoon',
+//            '/search-page',
+//            '/support',
+//            '/copyright',
+//            '/send-reset-password',
+//            '/404'
+//        ]));
+//        fclose($fp);
 
         $i = 0;
         Film::with(['translations'])
             ->orderBy('id', 'desc')
-            ->chunk(1000, function (Collection $films) use (&$i, $fileName) {
+            ->chunk(10, function (Collection $films) use (&$i, $path, $fileName) {
                 $data = '';
 
                 foreach ($films as $film) {
                     $data .= '/' . $film->category . '/' . $film->slug . "\r\n";
                 }
 
-                $fp = fopen($fileName, 'a+');
+                $fp = fopen($path . '/' . $fileName, 'a+');
                 fwrite($fp, $data);
                 fclose($fp);
 
-                dump('processed ' . ++$i * 1000);
+                $result = Process::forever()
+                    ->path($path)
+                    ->run('ng run front-end:prerender --routes-file ' . $fileName);
+
+                echo $result->output();
+                echo $result->errorOutput();
+
+                dump('processed ' . ++$i * 100);
             });
+
+
     }
 }
