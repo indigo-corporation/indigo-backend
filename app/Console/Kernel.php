0c262affd7b6e9684318517b4869162593bd5538
Kirill Yevchenko <k.yevchenko@ukr.net>
2023-06-26 06:51:07 +0300

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,14 +12,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('store-films film 1 2')->hourly();
-        $schedule->command('store-films serial 1 2')->hourly();
+        $schedule->command('store-films film')->hourly();
+        $schedule->command('store-films serial')->hourly();
+        $schedule->command('store-films anime')->hourly();
 
-        $schedule->command('store-films film 1 10')->daily()->at('00:40');
-        $schedule->command('store-films serial 1 10')->daily()->at('00:45');
+        $schedule->command('store-films film 1 2')->daily()->at('00:40');
+        $schedule->command('store-films serial 1 2')->daily()->at('00:45');
+        $schedule->command('store-films anime 1 2')->daily()->at('00:50');
 
         $schedule->command('backup:clean --disable-notifications')->daily()->at('01:00');
         $schedule->command('backup:run --only-db --disable-notifications')->daily()->at('01:02');
+
+        $schedule->command('update-sitemap')->daily()->at('01:05');
+        $schedule->command('prerender-routes')->daily()->at('01:07');
 
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
